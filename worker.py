18f5495a77b99d4aade0571e7d184720fd40764e
Risto Vuorio <vuoristo@gmail.com>
2017-07-22 15:23:56 +0900

    Drop support for tf 0.12.*

--- worker.py
+++ worker.py
@@ -8,7 +8,6 @@ from a3c import A3C
 from envs import create_env
 from arguments import args
 import distutils.version
-use_tf12_api = distutils.version.LooseVersion(tf.VERSION) >= distutils.version.LooseVersion('0.12.0')
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -25,14 +24,9 @@ def run(args, server):
     trainer = A3C(env, args.task, args.visualise)
 
     # Variable names that start with "local" are not saved in checkpoints.
-    if use_tf12_api:
-        variables_to_save = [v for v in tf.global_variables() if not v.name.startswith("local")]
-        init_op = tf.variables_initializer(variables_to_save)
-        init_all_op = tf.global_variables_initializer()
-    else:
-        variables_to_save = [v for v in tf.all_variables() if not v.name.startswith("local")]
-        init_op = tf.initialize_variables(variables_to_save)
-        init_all_op = tf.initialize_all_variables()
+    variables_to_save = [v for v in tf.global_variables() if not v.name.startswith("local")]
+    init_op = tf.variables_initializer(variables_to_save)
+    init_all_op = tf.global_variables_initializer()
     saver = FastSaver(variables_to_save)
 
     var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
@@ -47,10 +41,7 @@ def run(args, server):
     config = tf.ConfigProto(device_filters=["/job:ps", "/job:worker/task:{}/cpu:0".format(args.task)])
     logdir = os.path.join(args.log_dir, 'train')
 
-    if use_tf12_api:
-        summary_writer = tf.summary.FileWriter(logdir + "_%d" % args.task)
-    else:
-        summary_writer = tf.train.SummaryWriter(logdir + "_%d" % args.task)
+    summary_writer = tf.summary.FileWriter(logdir + "_%d" % args.task)
 
     logger.info("Events directory: %s_%s", logdir, args.task)
     sv = tf.train.Supervisor(is_chief=(args.task == 0),
